33c098dd0d80fcfad55c5809ee501ad1e69ee855
mina_72 <mina.mahdizadeh91@gmail.com>
2023-04-23 12:09:30 +0330

    create controller,module and service tasks

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksModule } from './tasks/tasks.module'
+
+
+
+@Module({
+  imports: [TasksModule],
+  controllers: [TasksController],
+
+})
+export class AppModule {}
